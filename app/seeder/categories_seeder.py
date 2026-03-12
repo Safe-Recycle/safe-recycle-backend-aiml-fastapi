f087f25a1288d1ce614287fcd4cd81fb478224c9
Ikram-sabila <ikramsabila@gmail.com>
2026-03-12 11:52:10 +0700

    Refactor code structure for improved readability and maintainability

--- app/seeder/categories_seeder.py
+++ app/seeder/categories_seeder.py
@@ -1,7 +1,7 @@
 import gdown
 import os
 
-from app.core.config import settings
+# from app.core.config import settings
 from app.databases.session import get_session
 from app.services.category_service import create_category
 from app.schemas.category_schema import CreateCategory
@@ -10,33 +10,33 @@ folder_path = "storage/image/categories"
 session = next(get_session())
 
 # Download files from google drive
-gdown.download_folder(
-    settings.GOOGLE_DRIVE_CATEGORIES, 
-    output=folder_path,
-    quiet=False
-)
+# gdown.download_folder(
+#     settings.GOOGLE_DRIVE_CATEGORIES, 
+#     output=folder_path,
+#     quiet=False
+# )
 
 # Remove files
-remove_files = [
-    'other.png',
-    'no-internet.png',
-    'no-results.png',
-    'non-recycleable.png',
-    'recycleable.png',
-    'non-reusable.png',
-    'other.png',
-    'reusable.png',
-    'x.png'
-]
+# remove_files = [
+#     'other.png',
+#     'no-internet.png',
+#     'no-results.png',
+#     'non-recycleable.png',
+#     'recycleable.png',
+#     'non-reusable.png',
+#     'other.png',
+#     'reusable.png',
+#     'x.png'
+# ]
 
-for filename in remove_files:
-    filepath = os.path.join(folder_path, filename)
+# for filename in remove_files:
+#     filepath = os.path.join(folder_path, filename)
     
-    if os.path.exists(filepath):
-        os.remove(filepath)
-        print(f"{filename} is removed")
-    else:
-        print(f"{filename} tidak ditemukan")
+#     if os.path.exists(filepath):
+#         os.remove(filepath)
+#         print(f"{filename} is removed")
+#     else:
+#         print(f"{filename} tidak ditemukan")
 
 # Seed data to database
 for file in os.listdir(folder_path):
